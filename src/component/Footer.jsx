e4c5dd5e01fa9011b4fec9072d30053dbaab27de
Om Vashishtha <143586605+omvashishtha@users.noreply.github.com>
2026-08-07 01:36:34 +0530

    Gh pages

---
+++ src/component/Footer.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import styles from '../component/css/Footer.module.css';
+import { FaLinkedin, FaInstagram } from "react-icons/fa";
+
+// 1. IMPORT THE IMAGE HERE
+import footerBrand from '../assets/footer-brand.png';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerTop}>
+        <div className={styles.footerCol}>
+          <h3 className={styles.footerHeading}>The Next Generation of Document Signature</h3>
+          <p className={styles.footerEmail}>gateway@nexgn.cloud</p>
+
+          <div className={styles.socialRow}>
+            <a href="https://www.linkedin.com/company/nexgncloud" target='_blank' rel="noreferrer" className={styles.socialIcon}><FaLinkedin /></a>
+            <a href="#!" className={styles.socialIcon}><FaInstagram /></a>
+          </div>
+
+          <div className={styles.legalText}>
+            <p>© 2026 Nexgn, Inc. All rights reserved.</p>
+            <p>Global Backing: New Mexico, USA</p>
+            <p>Engineering HQ: Bharat (India)</p>
+            <p>Globally Compliant (ESIGN, UETA, eIDAS, IT Act)</p>
+            <p>Data Sovereignty across North America & APAC</p>
+          </div>
+        </div>
+
+        <div className={styles.footerCol}>
+          <p className={styles.proudlyText}>Proudly</p>
+          <p className={styles.madeInBharat}>
+            <span className={styles.orangeText}>Made</span>{" "}
+            <span className={styles.blueText}>in</span>{" "}
+            <span className={styles.greenText}>Bharat</span>
+          </p>
+        </div>
+
+        <div className={styles.linkCol}>
+          <h4>COMPANY</h4>
+          <a href="#!">About Us</a>
+          <a href="#!">Contact Us</a>
+          <a href="#!">Blog</a>
+        </div>
+
+        <div className={styles.linkCol}>
+          <h4>RESOURCES</h4>
+          <a href="#!">Help center</a>
+          <a href="https://status.nexgn.cloud">System Status</a>
+          <a href="#!">Security</a>
+        </div>
+        
+
+        <div className={styles.linkCol}>
+          <h4>LEGAL & TRUST</h4>
+          <a href="#!">Trust & Compliance</a>
+          <a href="#!">Privacy Policy</a>
+          <a href="#!">Terms & Services</a>
+        </div>
+      </div>
+
+      <div className={styles.footerBrand}>
+        <img src={footerBrand} alt="Nexgn Logo" width="100%" height="100%" />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '../css/Landing.module.css'
+import styles from '../pages/css/Landing.module.css'
 
 const Navbar = () => {
   return (
